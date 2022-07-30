d3d047fb1a6c9339c0f0a275e83cd9268bba8d2f
cristiano francesco finotto <cristiano.finotto@edu.itspiemonte.it>
2022-07-30 18:00:22 +0200

    add mobile version

--- script/hamburger-menu.js
+++ script/hamburger-menu.js
@@ -1,7 +1,12 @@
 
 const hamburger = document.querySelector('.hamburger-menu img');
 const navListHamburger = document.querySelector('.nav-list-hamburger');
+const currentDate = document.querySelectorAll('#date');
 
 hamburger.addEventListener('click', () => {
     navListHamburger.classList.toggle('nav-list-hamburger--hide');
 });
+
+for (let i = 0; i < currentDate.length; i++) {
+    currentDate[i].textContent = new Date().getFullYear();
+}
